07ae86b88a261513d25dd5602bddc0b6c6a95779
bhoomika72 <bhoomika.soni.79@gmail.com>
2025-02-05 18:36:24 +0530

    changes done

--- test/test_smoke_data_check.py
+++ test/test_smoke_data_check.py
@@ -42,8 +42,7 @@ def test_column_case_sensitivity(spark):
 @pytest.mark.smoke
 def test_reading_invalid_file(spark):
     invalid_file_path = "C:\\Users\\admin\\OneDrive - TestPerform\\Desktop\\cicd\\invalid.csv"
-    try:
-        spark.read.csv(invalid_file_path, header=True, inferSchema=True)
-        pytest.fail("Expected an error due to missing file, but none occurred.")
-    except Exception as e:
-        assert "Path does not exist" in str(e)
+    
+    spark.read.csv(invalid_file_path, header=True, inferSchema=True)
+    pytest.fail("Expected an error due to missing file, but none occurred.")
+    
